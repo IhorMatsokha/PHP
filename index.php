6359a9c9c585d9bf22f690ff6b1a4c0874bba0bc
Ihor_M <igorro_1@meta.ua>
2023-04-05 18:59:29 +0300

    123

--- index.php
+++ index.php
@@ -1,58 +1,97 @@
-
 <?php
-//1) Визначити довжину рядка 'The length of the string is length'
-echo '1) Визначити довжину рядка "The length of the string is length"';
+
+//Завдання 1:
+//Створіть масив, який містить наступні властивості: ім'я (name), прізвище (surname),
+// вік (age) та середній бал (avg_grade). Після цього серіалізуйте його.
+// Відобразьте готовий результат на єкран.
+// З серіалізованої строки зробіть unserialize та виведіть результат на екран.
+echo 'Task_1';
 echo '<br>';
-echo ('Довжина рядка ' . (strlen('The length of the string is length')) . ' символи.');
+$array = [
+    "name" => "Ivan",
+    "surname" => "Pupckin",
+    "age" => 100,
+    "avg_grade" => 10,
+];
+
+$str = serialize($array);
+echo $str;
 echo '<br>';
-echo '<br>';
-//2) Видалення символів екранування з цього тесту 'This is a \'test\' string.'
-echo '2) Видалення символів екранування з цього тесту "This is a \'test\' string."';
-echo '<br>';
-echo ('Нова строка - ' . (stripslashes('This is a \'test\' string.')));
-echo '<br>';
-echo '<br>';
-//3) Визначення кількості символів до першого співпадіння з словом word з цього тексту 'Hello, world!'
-echo '3) Визначення кількості символів до першого співпадіння з словом word з цього тексту "Hello, world!"';
-echo '<br>';
-echo (strspn("Hello, world!", "world"));
-echo '<br>';
-echo '<br>';
-//4) Порівняння двох рядків один 'Hello' і 'hello!'
-echo '4) Порівняння двох рядків один "Hello"" і "hello!"';
-echo '<br>';
-echo (strcmp("Hello", "hello"));
+$arr = unserialize($str);
+var_dump($arr);
 echo '<br>';
 echo '<br>';
-//5) Пошук підрядка 'world' в рядку "Hello, world!"
-echo '5) Пошук підрядка "world" в рядку "Hello, world!"';
+
+//Завдання 2:
+//Створіть рядок "I love PHP". Використовуючи функцію explode,
+// розбийте цей рядок на окремі слова та виведіть їх на екран.
+echo 'Task_2';
 echo '<br>';
-echo (substr("Hello, world!", 6));
-echo '<br>';
-echo '<br>';
-//6) Форматування рядка за допомогою sprintf(). В рядку "My name is John, I'm 30 years old, and my height is 1.75 meters."
-//Зробити заміну в імені , кількість років і зріст.
-echo '6) Форматування рядка за допомогою sprintf(). В рядку "My name is John, I\'m 30 years old, and my height is 1.75 meters."';
-echo '<br>';
-echo 'Зробити заміну в імені , кількість років і зріст.';
-echo '<br>';
-$str = 'My name is John, I\'m 30 years old, and my height is 1.75 meters.';
-echo (sprintf($str, 6));
+$str_task_2 = 'I love PHP';
+$arr_task_2 = explode(' ', $str_task_2);
+var_dump($arr_task_2);
 echo '<br>';
 echo '<br>';
-//7) Визначити ,що слова вимовляються приблизно однаково Smith , Smyth, John, join
-echo '7) Визначити ,що слова вимовляються приблизно однаково Smith , Smyth, John, join';
+
+//Завдання 3:
+//Створіть рядок "i love php". Використовуючи функцію ucwords,
+//змініть першу літеру кожного слова на велику та виведіть рядок на екран.
+echo 'Task_3';
 echo '<br>';
-echo (soundex("Smith") == soundex("Smyth"));
+$str_task_3 = 'i love php';
+echo ucwords($str_task_3);
 echo '<br>';
-echo (soundex("John") == soundex("join"));
+echo '<br>';
+
+//Завдання 4:
+//Створіть рядок " i love php ".
+// Використовуючи функцію trim,
+// видаліть пробіли в початку та в кінці рядка та виведіть рядок на екран.
+echo 'Task_4';
+echo '<br>';
+$str_task_4 = ' i love php     ';
+echo trim($str_task_4);
 echo '<br>';
 echo '<br>';
-//8) Заміни символів, які можуть бути сприйняті як HTML-теги з цієї строки <script>alert('test');</script>
-echo '8) Заміни символів, які можуть бути сприйняті як HTML-теги з цієї строки <script>alert(\'test\');</script>';
+
+//Завдання 5:
+//Створіть рядок "i love php". Використовуючи функцію substr, виведіть на екран слово "php".
+echo 'Task_5';
 echo '<br>';
-$str_1 = '<script>alert(\'test\');</script>';
-echo (htmlspecialchars($str_1));
+$str_task_5 = 'i love php';
+echo substr($str_task_5, -3);
+echo '<br>';
+echo '<br>';
+
+//Завдання 6:
+//Створіть рядок "Hello my world".
+// Використовуючи функцію substr, виведіть на екран слова "Hello world".
+// Слово "my" потрібно замінити на порожнечу.
+echo 'Task_6';
+echo '<br>';
+$str_task_6 = 'Hello my world';
+echo substr($str_task_6, 0, 5) . substr($str_task_6, 8, 6);
+echo '<br>';
+echo '<br>';
+
+//Завдання 7:
+//Розбити рядок "I love PHP" за допомогою циклу for.
+echo 'Task_7';
+echo '<br>';
+$str_task_7 = 'I love PHP';
+for ($i = 0;$i < Strlen($str_task_7);$i++){
+    echo $str_task_7[$i];
+    echo '<br>';
+}
+echo '<br>';
+echo '<br>';
+
+//Завдання 8:
+//Створіть рядок "i love php". Використовуючи функцію substr, виведіть на екран слово "love".
+echo 'Task_8';
+echo '<br>';
+$str_task_8 = 'i love php';
+echo substr($str_task_8, 2, 4);
+echo '<br>';
 echo '<br>';
 ?>
-
